fd1ea8584ff42508db5114854acd0889f7a11897
fennecJ <hwahwa649@gmail.com>
2021-03-26 02:57:53 +0800

    Mergesort work well now

--- hw1/merge_sort.c
+++ hw1/merge_sort.c
@@ -1,35 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void split(int* a,int len){
-int c = len / 2;
-int* l = malloc(sizeof(int)*c);
-int* r = malloc(sizeof(int)*(len-c));
+int* merge(int* l,int l_len,int* r,int r_len){
+int len = l_len + r_len;
+int *tmp = malloc(sizeof(int)*len);
 int i,j,k;
 i = j = k = 0;
-for(;i < len; i++){
-if(i<c)
-l[j++] = a[i];
+for( ; i < len ; i++){
+if(j == l_len || k== r_len)
+break;
+if(l[j]<r[k])
+tmp[i] = l[j++];
 else
-r[k++] = a[i];
-}
-printf("---------------------\n");
-i = 0;
-for(; i < len; i++){
-if(i<c)
-printf("%d " ,l[i]);
-else
-printf("%d " ,r[i-c]);
-}
+tmp[i] = r[k++];
 
 }
-
-int* merge(int* a,int* b){
-
+for(;j<l_len;j++)
+tmp[i++] = l[j];
+for(;k<r_len;k++)
+tmp[i++] = r[k];
+return tmp;
 }
+int* split(int* a,int len){
+    if(len<2)
+    return a;
+int c = len / 2;
+int* l = a;
+int* r = &(a[c]);
+int* ll = split(l,c);
+int* rr = split(r,len-c);
+return  merge(ll,c,rr,len-c);
+}
+
+
 int main(){
-int a[9]={0,1,2,3,4,5,6,7,8};
-split(a,9);
-
-
+int len = 10;
+int a[10]={5,1,3,4,7,10,15,6,9,8};
+int* b = split(a,len);
+int i = 0;
+printf("test");
+for( ; i < len ; i++)
+printf("%d ",b[i]);
+return 0;
 }
