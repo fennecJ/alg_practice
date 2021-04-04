14163e57e3a0d618b7d61d7cebf9dbb6e1bb0f2b
fennecJ <hwahwa649@gmail.com>
2021-04-05 03:54:56 +0800

    Now insertion sort work well

--- hw2/insertion_sort.c
+++ hw2/insertion_sort.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+void insertion(int *arr,int len){
+    for(int i = 1; i <len; i++){
+       int key = arr[i] ;
+       int j=i-1 ;
+       while(j>=0&&arr[j]>key){
+            arr[j+1]=arr[j];
+            j=j-1;
+            }
+        arr[j+1] = key;
+    }
 
+}
 int main(){
 FILE* fi;
 FILE* fo;
 fi=fopen("input.txt","r");
 fo=fopen("output.txt","w");
+int a[9]={5,4,3,2,1,1,5,7,6};
+insertion(a,9);
+for(int i = 0;i<9;i++)
+printf("%d ",a[i]);
+printf("\n");
 }
