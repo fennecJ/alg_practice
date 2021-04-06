0d976567207b4cf96ec8d47944d976ce9c7dda81
fennecJ <hwahwa649@gmail.com>
2021-04-06 20:20:27 +0800

    Now you can gen a random array with a parameter

--- hw2/gendata.c
+++ hw2/gendata.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
 void swap(int *a,int *b){
     if(a==b)
     return;
@@ -7,16 +8,26 @@ void swap(int *a,int *b){
     (*b)^=(*a);
     (*a)^=(*b);
 }
-int main(){
+int main(int argc){
+
+
+
 FILE* fo;
 fo=fopen("input.txt","w");
 int len = 10000;
 int arr[len];
+if(argc==1){
 for(int i = 0;i<len;i++)
     arr[i]=i+1;
 for(int i = 0;i<len;i++)
     if((i+1)%400==0)
         swap(arr+i,arr+i-199);
+        }
+else{     
+ srand(time(NULL));
+ for(int i = 0;i<len;i++)
+  arr[i]=(rand()%10000);
+}
 fprintf(fo,"%d\n",len);
 for(int i = 0; i < len ; i++)
 fprintf(fo,"%d\n",arr[i]);
