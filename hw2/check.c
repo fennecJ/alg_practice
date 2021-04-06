f6d29abc16ae2088e54c40848126a09c4d3e1596
fennecJ <hwahwa649@gmail.com>
2021-04-06 17:56:42 +0800

    Add check.c to check if output is wrong

---
+++ hw2/check.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+FILE* fi;
+fi=fopen("output.txt","r");
+int len;
+fscanf(fi,"%d\n",&len);
+int* a = malloc(sizeof(int)*len);
+int i = 0;
+while(fscanf(fi,"%d\n",a+i)!=EOF)
+i++;
+for(int j = 1;j<len;j++)
+if(a[j-1]>a[j])
+printf("Error at %d!\n",j+2);
+}
