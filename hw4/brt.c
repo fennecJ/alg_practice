fe3b34cead210e2b28a81d3dd2fc5f51e40227df
fennecJ <hwahwa649@gmail.com>
2021-04-21 10:18:23 +0800

    Fix some issue
    Delete several unused code
    Implement of sel funct
    It seems FIX cannot colored the tree well

--- hw4/brt.c
+++ hw4/brt.c
@@ -14,7 +14,12 @@
 #endif
 #define RED true
 #define BLK false
-
+#define INFO \
+"Available commands:\n\
+i {int} - insert node with key {int} into tree\n\
+graph - print the tree in 2-D\n\
+help - print the help\nq - exit\n\
+"
 struct node
 {
     struct node* left;
@@ -29,33 +34,38 @@ struct tree{
     node_t* root;
     node_t* nil;
 };
-
 typedef struct tree tree_t;
 node_t* new_node(tree_t* T,int k);
 tree_t* new_tree();
 node_t* search(tree_t* T,node_t* head,int x);
-void insert(tree_t* T,int x);
 void print_tree(tree_t* T,node_t* root, int i,int tar);
-node_t* max_node(node_t* head);
-node_t* min_node(node_t* head);
-node_t* suc(tree_t* T,node_t* head);
-node_t* pred(tree_t* T,node_t* head);
-void transp(tree_t* T,node_t* u,node_t* v);
-void delete(tree_t* T,node_t* z);
 node_t* parent(tree_t* T,node_t* tar);
 void l_rotate(tree_t* T,node_t* x);
 void r_rotate(tree_t* T,node_t* x);
 void rb_insert(tree_t* T,node_t* z);
 //void mod_parent(tree_t* T,node_t* tar,node_t* post);
+node_t* sel(tree_t* T,node_t* x,int i){
+    if(x==T->nil){
+    return T->nil;
+    printf("Cannot find %d-th smallest key in RBT\n",i);
+    }
+    int r = x->left->cnt+1;
+    if(i==r)
+        return x;
+    else if(i<r)
+        return sel(T,x->left,i);
+    else
+        return sel(T,x->right,i-r);
+}
+
 int cmd_parse(char* cmd);
 void FIX(tree_t* T,node_t* z);
-
 int main(){
 tree_t* T=new_tree();
 char cmd[10];
 int tmp;
 node_t* t;
-printf("Available commands:\ns {int} - search for specific node with selected key {int}\nmax - return max from tree\nmin - return min from tree\nsu {int} - search for successor of specific node with selected key {int}\npr {int} - search for predecessor of specific node with selected key {int}\ni {int} - insert node with key {int} into tree\nd {int} - delete specific node with selected key {int}\npar {int} - return the parent of specific node with selected key {int}\nhelp - print the help\nq - exit\n");
+printf(INFO);
 while(1){
 scanf("%s",cmd);
 cmd[9]='\0';
@@ -67,107 +77,21 @@ switch (c)
     {   
         case 1:
             scanf("%d",&tmp);
-            printf("Finding %d in tree\n",tmp);
-            if(search(T,T->root,tmp))
-            print_tree(T,T->root,0,tmp);
-            else
-            printf("%d was not found!!\n",tmp);
-            break;
-        case 2:
-            printf("Finding max element in tree\n");
-            if(T->root){
-            t=max_node(T->root);
-            printf("Max element is %d\n",t->key);
-            print_tree(T,T->root,0,t->key);
-            }else
-            printf("The tree was empty!!\n");
-        break;
-        case 3:
-            printf("Finding min element in tree\n");
-            if(T->root){
-            t=min_node(T->root);
-            printf("min element is %d\n",t->key);
-            print_tree(T,T->root,0,t->key);
-            }else
-            printf("The tree was empty!!\n");
-        break;
-        case 4:
-            scanf("%d",&tmp);
-            printf("Finding successor of %d in tree\n",tmp);
-            t=search(T,T->root,tmp);
-            if(t){
-                t=suc(T,t);
-                if(t){
-                printf("Successor of %d in tree: %d\n",tmp,t->key);
-                print_tree(T,T->root,0,t->key);
-                }else
-                printf("%d have no successor!!\n",tmp);
-            }else
-            printf("%d was not in the tree!!\n",tmp);
-        break;
-        case 5:
-            scanf("%d",&tmp);
-            printf("Finding predecessor of %d in tree\n",tmp);
-            t=search(T,T->root,tmp);
-            if(t){
-                t=pred(T,t);
-                if(t){
-                printf("Predecessor of %d in tree: %d\n",tmp,t->key);
-                print_tree(T,T->root,0,t->key);
-                }else
-                printf("%d have no predecessor!!\n",tmp);
-            }else
-            printf("%d was not in the tree!!\n",tmp);
-        break;
-        case 6:
-            scanf("%d",&tmp);
             printf("Insert %d into tree\n",tmp);
             t=new_node(T,tmp);
-
             rb_insert(T,t);
             print_tree(T,T->root,0,t->key);
         break;
-        case 7:
+        case 2:
             scanf("%d",&tmp);
-            printf("Delete %d from tree\n",tmp);
-            t=search(T,T->root,tmp);
-            if(t!=T->nil)
-            if(t->cnt>1){
-            t->cnt--;
-            print_tree(T,T->root,0,tmp);
-            }
-            else{
-                delete(T,t);
-                print_tree(T,T->root,0,tmp);
-            }else
-            printf("%d was not in the tree!!\n",tmp);
-        break;
-        case 8:
-            scanf("%d",&tmp);
-            printf("Finding parent of %d in tree\n",tmp);
-            node_t* p=parent(T,search(T,T->root,tmp));
-            if(p!=T->nil)
-            print_tree(T,T->root,0,p->key);
-            else
-            printf("Parent not found\n");
-        break;
-        case 9:
-            printf("Available commands:\ns {int} - search for specific node with selected key {int}\nmax - return max from tree\nmin - return min from tree\nsu {int} - search for successor of specific node with selected key {int}\npr {int} - search for predecessor of specific node with selected key {int}\ni {int} - insert node with key {int} into tree\nd {int} - delete specific node with selected key {int}\npar {int} - return the parent of specific node with selected key {int}\nhelp - print the help\nq - exit\n");
-            break;
-        case 10:
-            scanf("%d",&tmp);
-            printf("left-rotate at %d\n",tmp);
-            t = search(T,T->root,tmp);
-            l_rotate(T,t);
+            printf("Sel %d-th smallest key from tree\n",tmp);
+            t=sel(T,T->root,tmp);
+            printf("%d-th smallest key was found at %d\n",tmp,t->key);
             print_tree(T,T->root,0,t->key);
-            break;
-        case 11:
-            scanf("%d",&tmp);
-            printf("right-rotate at %d\n",tmp);
-            t = search(T,T->root,tmp);
-            r_rotate(T,t);
-            print_tree(T,T->root,0,t->key);
-            break;    
+        break;
+        case 6:
+            printf(INFO);
+        break;   
     default:
         printf("Invalid command, type help to see available operations\n");
         break;
@@ -277,28 +201,6 @@ T->root->color=BLK;
 
 
 
-
-void insert(tree_t* T,int x){
-    node_t* head=T->root;
-    node_t* ins=new_node(T,x);
-    if(!head){
-    T->root=ins;
-    return;
-    }
-    node_t* tmp=NULL;
-    while(head!=T->nil){
-        tmp = head;
-        if(x<head->key)
-        head=head->left;
-        else
-        head=head->right;
-    }
-    if(tmp->key>x)
-    tmp->left=ins;
-    else
-    tmp->right=ins;
-}
-
 void print_tab(int i){
     for(int j = 0;j<i;j++)
     printf("|\t");
@@ -315,62 +217,29 @@ void print_tree(tree_t* T,node_t* root, int i,int tar){
         print_tab(i-1);
             if(root->key==tar){
                 if(root->color==RED)
-                printf("|======="ANSI_COLOR_RED"{%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+                printf("|======="ANSI_COLOR_RED"{*%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
                 else
                 printf("|======="ANSI_COLOR_BLUE"{%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
             }
             else if(root->color==RED)
-            printf("|======="ANSI_COLOR_RED"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
-            else printf("|======="ANSI_COLOR_BLUE"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+            printf("|======="ANSI_COLOR_RED"*%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+            else 
+            printf("|======="ANSI_COLOR_BLUE"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
             
     }else{
             if(root->key==tar){
                 if(root->color==RED)
-                printf(ANSI_COLOR_RED"{%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+                printf(ANSI_COLOR_RED"{*%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
                 else
                 printf(ANSI_COLOR_BLUE"{%d[%d]}"ANSI_COLOR_RESET"\n",root->key,root->cnt);
             }
             else if(root->color==RED)
-            printf(ANSI_COLOR_RED"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
-            else printf(ANSI_COLOR_BLUE"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+            printf(ANSI_COLOR_RED"*%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
+            else 
+            printf(ANSI_COLOR_BLUE"%d[%d]"ANSI_COLOR_RESET"\n",root->key,root->cnt);
     }
     print_tree(T,root->left,i+1,tar);
 }
-
-node_t* max_node(node_t* head){
-    while(head->right)
-    head=head->right;
-    return head;
-}
-
-node_t* min_node(node_t* head){
-    while(head->left)
-    head=head->left;
-    return head;
-}
-
-node_t* suc(tree_t* T,node_t* head){
-    if(head->right)
-        return min_node(head->right);
-    node_t* y = parent(T,head);
-    while(y&&head==y->right){
-        head=y;
-        y=parent(T,y);
-    }
-    return y;
-}
-
-node_t* pred(tree_t* T,node_t* head){
-    if(head->left)
-        return max_node(head->left);
-    node_t* y = parent(T,head);
-    while(y&&head==y->left){
-        head=y;
-        y=parent(T,y);
-    }
-    return y;
-}
-
 
 node_t* parent(tree_t* T,node_t* tar){
     node_t* head=T->root;
@@ -387,35 +256,9 @@ node_t* parent(tree_t* T,node_t* tar){
     return T->nil;
 }
 
-void transp(tree_t* T,node_t* u,node_t* v){
-    if(parent(T,u)==NULL)
-        T->root=v;
-    else if(u==(parent(T,u)->left))
-    parent(T,u)->left=v;
-    else
-    parent(T,u)->right=v;
-}
-
-void delete(tree_t* T,node_t* z){
-    if(z->left==NULL)
-        transp(T,z,z->right);
-    else if(z->right==NULL)
-        transp(T,z,z->left);
-    else{
-        node_t* y = min_node(z->right);
-        if(parent(T,y)!=z){
-            transp(T,y,y->right);
-            y->right=z->right;
-        }
-        transp(T,z,y);
-        y->left=z->left;
-    }
-    free(z);
-}
-
 
 void l_rotate(tree_t* T,node_t* x){
-    //printf("Call l_rotate at %d\n",x->key);
+    printf("Call l_rotate at %d\n",x->key);
     node_t* y=x->right;
     x->right=y->left;
     if(parent(T,x)==T->nil)
@@ -431,7 +274,7 @@ void l_rotate(tree_t* T,node_t* x){
 
 
 void r_rotate(tree_t* T,node_t* x){
-    //printf("Call r_rotate at %d\n",x->key);
+    printf("Call r_rotate at %d\n",x->key);
     node_t* y=x->left;
     x->left=y->right;
     if(parent(T,x)==T->nil)
@@ -450,28 +293,18 @@ void r_rotate(tree_t* T,node_t* x){
 
 
 int cmd_parse(char* cmd){
-    if(strcmp(cmd,"s")==0)
-        return 1;
-    if(strcmp(cmd,"max")==0)
-        return 2;
-    if(strcmp(cmd,"min")==0)
-        return 3;
-    if(strcmp(cmd,"su")==0)
-        return 4;
-    if(strcmp(cmd,"pr")==0)
-        return 5;
     if(strcmp(cmd,"i")==0)
-        return 6;
-    if(strcmp(cmd,"d")==0)
-        return 7;
-    if(strcmp(cmd,"par")==0)
-        return 8;
+        return 1;
+    if(strcmp(cmd,"sel")==0)
+        return 2;
+    if(strcmp(cmd,"rank")==0)
+        return 3;
+    if(strcmp(cmd,"pr")==0)
+        return 4;
+    if(strcmp(cmd,"graph")==0)
+        return 5;
     if(strcmp(cmd,"help")==0)
-        return 9;
-    if(strcmp(cmd,"lr")==0)
-        return 10;
-    if(strcmp(cmd,"rr")==0)
-        return 11;
+        return 6;
     return -1;
 }
 
