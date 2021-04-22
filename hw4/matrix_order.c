5300366aa7a771944e185351d5ec65dc35dc815e
fennecJ <hwahwa649@gmail.com>
2021-04-22 21:50:04 +0800

    Fix several errors
    Now matrix_order worked well

--- hw4/matrix_order.c
+++ hw4/matrix_order.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 void MCO(int *a, int **m, int **s, int len);
-void POP(int **s, int i, int j);
+void POP(int **s, int i, int j, FILE *fo);
 int main() {
-
     FILE *fi;
     FILE *fo;
     fi = fopen("input.txt", "r");
@@ -20,7 +19,6 @@ int main() {
     for (k = 0; k < len; k++) {
         fscanf(fi, "%d\n", a + k);
     }
-    // int m[len][len], s[len][len];
     int **m, **s;
     m = (int **)malloc(sizeof(int *) * len);
     s = (int **)malloc(sizeof(int *) * len);
@@ -28,34 +26,32 @@ int main() {
         m[k] = (int *)malloc(sizeof(int) * len);
         s[k] = (int *)malloc(sizeof(int) * len);
     }
-    // printf("1\n");
     MCO(a, m, s, len);
-    // printf("2\n");
+    /* Visualize the result of s
     for (int i = 1; i < len; i++) {
-        for (int j = i; j < len; j++)
+        for (int k = 0; k < i; k++)
+            printf("  ");
+        for (int j = i; j < len; j++) {
             printf("%d ", s[i][j]);
+        }
         printf("\n");
     }
-
-    POP(s, 1, len - 1);
+    */
+    fprintf(fo, "%d\n", m[1][len - 1]);
+    POP(s, 1, len - 1, fo);
     printf("\n");
-    /*
-    while(fscanf(fi,"%d\n",a+i)!=EOF)
-    i++;*/
-
-    // while(fscanf(fi,"%d\n",a+i)!=EOF)
 }
 
 void MCO(int *a, int **m, int **s, int len) {
     int n = len;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
         m[i][i] = 0;
-    for (int l = 1; l < n; l++) {
-        for (int i = 0; i + l < n; i++) {
-            int j = i + l;
+    for (int l = 2; l < n; l++) {
+        for (int i = 1; i < n - l + 1; i++) {
+            int j = i + l - 1;
             m[i][j] = INT_MAX;
             for (int k = i; k < j; k++) {
-                int q = m[i][k] + m[k + 1][j] + a[i] * a[k] * a[j];
+                int q = m[i][k] + m[k + 1][j] + a[i - 1] * a[k] * a[j];
                 if (q < m[i][j]) {
                     m[i][j] = q;
                     s[i][j] = k;
@@ -64,13 +60,13 @@ void MCO(int *a, int **m, int **s, int len) {
         }
     }
 }
-void POP(int **s, int i, int j) {
+void POP(int **s, int i, int j, FILE *fo) {
     if (i == j)
-        printf("A%d", i);
+        fprintf(fo, "A%d", i);
     else {
-        printf("(");
-        POP(s, i, s[i][j]);
-        POP(s, s[i][j] + 1, j);
-        printf(")");
+        fprintf(fo, "(");
+        POP(s, i, s[i][j], fo);
+        POP(s, s[i][j] + 1, j, fo);
+        fprintf(fo, ")");
     }
 }
