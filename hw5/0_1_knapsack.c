6c155e57630dec6d0a2bcc3dbbc4933ff2725433
fennecJ <hwahwa649@gmail.com>
2021-05-17 21:40:29 +0800

    Scan the input file worked well

--- hw5/0_1_knapsack.c
+++ hw5/0_1_knapsack.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
     FILE *fi;
     FILE *fo;
     fi = fopen("input.txt", "r");
     fo = fopen("output.txt", "w");
+    int items, max_weight;
+    fscanf(fi, "%d", &items);
+    fscanf(fi, "%d", &max_weight);
+    int **ref = calloc(1, sizeof(int *) * items);
+    for (int i = 0; i < items; i++)
+        ref[i] = calloc(1, sizeof(int) * 2);
+    for (int i = 0; i < items; i++)
+        fscanf(fi, "%d %d", &ref[i][0], &ref[i][1]);
+
+    // ref[items][0] = item's value
+    // ref[items][1] = item's weight
 }
